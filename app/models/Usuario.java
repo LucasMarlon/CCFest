5e0349d4f812fc110a39967f31334d0d571c2ece
Lucas Marlon <lucas.marques@ccc.ufcg.edu.br>
2014-08-17 15:34:07 -0300

    equals e hashCode

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -54,11 +54,39 @@ public class Usuario {
 	}
 	
 	@Override
-	public boolean equals(Object obj){
-		if(obj == null || !(obj instanceof Usuario)){
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
 			return false;
-		}
-		Usuario outro = (Usuario) obj;
-		return this.getEmail().equals(outro.getEmail());
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (pass == null) {
+			if (other.pass != null)
+				return false;
+		} else if (!pass.equals(other.pass))
+			return false;
+		return true;
 	}
 }
